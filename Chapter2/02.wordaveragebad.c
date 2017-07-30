ad214f92f61522a586e5dd6541101b644d20fefa
HardPlant <abc7988se@naver.com>
2017-07-30 00:58:44 -0700

    now thread_safe

--- Chapter2/02.wordaveragebad.c
+++ Chapter2/02.wordaveragebad.c
@@ -1,13 +1,15 @@
 #include <string.h>
+//char *strtok_r(char* s, const char* delim, char** save_ptr)
 #define LINE_DELIMITERS "\n"
 #define WORD_DELIMITERS " "
 
 static int wordcount(char *s){
     int count = 1;
+    char* save_ptr;
 
-    if(strtok(s, WORD_DELIMITERS) == NULL)
+    if(strtok_r(s, WORD_DELIMITERS, save_ptr) == NULL)
         return 0;
-    while(strtok(NULL, WORD_DELIMITERS)) // not safe; this func uses static
+    while(strtok_r(NULL, WORD_DELIMITERS, save_ptr))
         count++;
     return count;
 }
