035d7db002db8aa73d42a60feb6a941c4f32115e
HardPlant <abc7988se@naver.com>
2017-07-30 01:06:07 -0700

    bubble sort interchange count function.

---
+++ Chapter2/03.bubblesort.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+static int count = 0;
+static int onepass(int a[], int n){
+    int i;
+    int interchanges = 0;
+    int temp;
+    for (i=0; i<n-1; i++)
+    if (a[i] > a[i+1]){
+        temp = a[i];
+        a[i] = a[i+1];
+        a[i+1] = temp;
+        interchanges = 1;
+        count++; // static int
+    }
+    return interchanges;
+}
