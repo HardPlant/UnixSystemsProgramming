03c93c7e7f4275df54a8ee32d0e76a4cf02c490a
HardPlant <abc7988se@naver.com>
2017-07-31 05:38:01 -0700

    remove

--- Chapter2/06.get_PATH_argv.c
+++ Chapter2/06.get_PATH_argv.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define PATH_DELIMITERS ":"
+
 int makeargv(const char *s, const char* delimiters, char*** argvp);
-extern char **environ;
+
 char** getpath(void){
     char** myargv;
     char* path;
